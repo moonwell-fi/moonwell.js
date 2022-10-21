0a88fc07b9102c0f8eb06d7e6895463b8af56770
Octavius <97316189+moonwell-octavius@users.noreply.github.com>
2022-10-21 13:24:16 -0400

    Fix tests and deploy docs

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,12 @@
-import {moonwellContracts} from "../src";
+import {Contracts} from "../src";
 
 test("contracts", () => {
   // Ensure we have some expected value and it's well formed
-  expect(moonwellContracts.moonbase.COMPTROLLER).toMatch(/^0x\w{40}/)
+  expect(Contracts.moonbase.COMPTROLLER).toMatch(/^0x\w{40}/)
+
+  expect(Contracts.moonriver.COMPTROLLER)
+      .not.toMatch(Contracts.moonbase.COMPTROLLER)
+
+  expect(Contracts.moonriver.COMPTROLLER)
+      .not.toMatch(Contracts.moonbeam.COMPTROLLER)
 });
