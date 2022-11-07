f8ed92ea428bcb18d36089aef368b3b078fa2749
Lunar Engineering <lunar-eng@moonwell.fi>
2022-11-06 18:12:16 -0800

    more changes

--- src/environments/moonbase.ts
+++ src/environments/moonbase.ts
@@ -1,79 +1,124 @@
 import BigNumber from "bignumber.js";
 
 import { ContractBundle } from "../types";
+import { MoonwellContract, MoonwellContractWithProxy } from '../contracts'
 
-// export const contracts: ContractBundle = {
-//     CLAIMS: '0xe7e6cdb90797f053229c0a81c3de9dc8110188b5',
-//     COMPTROLLER: '0x4524e0Dfa042188B1F9f804642456277dbDBE253',
-//     GOVERNOR: '0x1eA239b116b911374BA144EFb5cA077d3A224fE9',
-//     GOV_TOKEN: '0x6efCf8fB404AC6DE593cE461877A634d067C942E',
-//     MAXIMILLION: '0xa2A09f11208327e69Ede9c2C6eE95282a13527AF',
-//     ORACLE: '0x0A3F1AA8bc5C51bbD313e63644f2297aA170FF87',
-//     SAFETY_MODULE: '0x11fD9c97B0B8F50f6EB0e68342e3de8F76dd45fc',
-//     TIMELOCK: '0xE2270EEf667A66AF330f8411C17E79BdA2FC3595',
-//     INTEREST_RATE_MODEL: '0x4BABE1d4f89CbDdA49f0402B2C39d0c1923BF4A7',
-//     MERC_20_IMPL: '0x83C5dE4dDc7bE93907Cb599aCFd2db47838e00df',
-//
-//     MARKETS: {
-//         "BTC.dev": {
-//             name: "Bitcoin",
-//             assetTicker: "BTC",
-//             tokenAddress: '0x0A996B2f808EF4Cd59a3790c6Bb8bf6E4105e0aA',
-//             mTokenAddress: "0x4b088a4b3250684c362911078A0E945AcC21E236",
-//             digits: 8,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e8),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "USDC.dev": {
-//             name: "USD Coin",
-//             assetTicker: "USDC",
-//             tokenAddress: '0x3799D95Ee109129951c6b31535b2B5AA6dbF108c',
-//             mTokenAddress: "0x18f324E21846F1C21F4fbF8228705B17897eF15A",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e6),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "ETH.dev": {
-//             name: "Ethereum",
-//             assetTicker: "ETH",
-//             tokenAddress: '0xf7A200a738f807B78c7a60aA315e115612C6c33a',
-//             mTokenAddress: "0xa28C4680058f4A73f6172A6ed23C9E624E443CFB",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e18),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "DEV": {
-//             name: "DEV",
-//             assetTicker: "DEV",
-//             tokenAddress: null,
-//             mTokenAddress: "0x827Bb1E617B862d4BC81a6a7995a34dF6EE78a63",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e18),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "FRAX": {
-//             name: "Frax",
-//             assetTicker: "FRAX",
-//             tokenAddress: '0x90384c78892be628DF6C76c3E49714Ef719117f4',
-//             mTokenAddress: "0x241D3210B0706e0c40CE5274bc23a9E494162F5F",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e18),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "USDT.dev": {
-//             name: "Tether",
-//             assetTicker: "USDT",
-//             tokenAddress: '0xDc67f6725B90B7e623f008028835B00F8c399FE3',
-//             mTokenAddress: "0x1C3132CF5e41Eb3660B403db494711Ac5F383125",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e6),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//     }
-// }
+export const contracts: ContractBundle = {
+    CLAIMS: new MoonwellContractWithProxy(
+        '0xe7e6cdb90797f053229c0a81c3de9dc8110188b5',
+        './deploy-artifacts/TokenSaleDistributor.json',
+        './deploy-artifacts/TokenSaleDistributorProxy.json',
+    ),
+    GOVERNOR: new MoonwellContract(
+        '0x1eA239b116b911374BA144EFb5cA077d3A224fE9',
+        './deploy-artifacts/MoonwellGovernorArtemis.json',
+    ),
+
+    COMPTROLLER: new MoonwellContractWithProxy(
+        '0x4524e0Dfa042188B1F9f804642456277dbDBE253', 
+        './deploy-artifacts/Comptroller.json',
+        './deploy-artifacts/Unitroller.json',
+    ),
+
+
+    GOV_TOKEN:  new MoonwellContract(
+        '0x6efCf8fB404AC6DE593cE461877A634d067C942E', 
+        './deploy-artifacts/Well.json'
+    ),
+
+
+    MAXIMILLION: new MoonwellContract(
+        '0xa2A09f11208327e69Ede9c2C6eE95282a13527AF', 
+        './deploy-artifacts/Maximillion.json'
+    ),
+
+    ORACLE: new MoonwellContract(
+        '0x0A3F1AA8bc5C51bbD313e63644f2297aA170FF87', 
+        './deploy-artifacts/ChainlinkOracle.json'
+    ),
+
+
+    SAFETY_MODULE: new MoonwellContractWithProxy(
+        '0x11fD9c97B0B8F50f6EB0e68342e3de8F76dd45fc', 
+        './deploy-artifacts/StakedWell.json',
+        './deploy-artifacts/TransparentProxy.json',
+    ),
+
+    TIMELOCK: new MoonwellContract(
+        '0xE2270EEf667A66AF330f8411C17E79BdA2FC3595', 
+        './deploy-artifacts/Timelock.json',
+    ),
+
+    INTEREST_RATE_MODEL: new MoonwellContract(
+        '0x4BABE1d4f89CbDdA49f0402B2C39d0c1923BF4A7', 
+        './deploy-artifacts/StakedWell.json',
+    ),
+
+    MERC_20_IMPL: new MoonwellContract(
+        '0x83C5dE4dDc7bE93907Cb599aCFd2db47838e00df', 
+        './deploy-artifacts/MErc20Delegator.json',
+    ),
+
+    MARKETS: {
+        "BTC.dev": {
+            name: "Bitcoin",
+            assetTicker: "BTC",
+            tokenAddress: '0x0A996B2f808EF4Cd59a3790c6Bb8bf6E4105e0aA',
+            mTokenAddress: "0x4b088a4b3250684c362911078A0E945AcC21E236",
+            digits: 8,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e8),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "USDC.dev": {
+            name: "USD Coin",
+            assetTicker: "USDC",
+            tokenAddress: '0x3799D95Ee109129951c6b31535b2B5AA6dbF108c',
+            mTokenAddress: "0x18f324E21846F1C21F4fbF8228705B17897eF15A",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e6),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "ETH.dev": {
+            name: "Ethereum",
+            assetTicker: "ETH",
+            tokenAddress: '0xf7A200a738f807B78c7a60aA315e115612C6c33a',
+            mTokenAddress: "0xa28C4680058f4A73f6172A6ed23C9E624E443CFB",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e18),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "DEV": {
+            name: "DEV",
+            assetTicker: "DEV",
+            tokenAddress: null,
+            mTokenAddress: "0x827Bb1E617B862d4BC81a6a7995a34dF6EE78a63",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e18),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "FRAX": {
+            name: "Frax",
+            assetTicker: "FRAX",
+            tokenAddress: '0x90384c78892be628DF6C76c3E49714Ef719117f4',
+            mTokenAddress: "0x241D3210B0706e0c40CE5274bc23a9E494162F5F",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e18),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "USDT.dev": {
+            name: "Tether",
+            assetTicker: "USDT",
+            tokenAddress: '0xDc67f6725B90B7e623f008028835B00F8c399FE3',
+            mTokenAddress: "0x1C3132CF5e41Eb3660B403db494711Ac5F383125",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e6),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+    }
+}
--- src/environments/moonriver.ts
+++ src/environments/moonriver.ts
@@ -1,89 +1,134 @@
 import BigNumber from "bignumber.js";
 
 import { ContractBundle } from "../types";
+import { MoonwellContract, MoonwellContractWithProxy } from '../contracts'
 
-// export const contracts: ContractBundle = {
-//     CLAIMS: '0x8568A675384d761f36eC269D695d6Ce4423cfaB1',
-//     COMPTROLLER: '0x0b7a0EAA884849c6Af7a129e899536dDDcA4905E',
-//     GOVERNOR: '0x2BE2e230e89c59c8E20E633C524AD2De246e7370',
-//     GOV_TOKEN: '0xBb8d88bcD9749636BC4D2bE22aaC4Bb3B01A58F1',
-//     MAXIMILLION: '0x1650C0AD9483158f9e240fd58d0E173807A80CcC',
-//     ORACLE: '0x892bE716Dcf0A6199677F355f45ba8CC123BAF60',
-//     SAFETY_MODULE: '0xCd76e63f3AbFA864c53b4B98F57c1aA6539FDa3a',
-//     TIMELOCK: '0x04e6322D196E0E4cCBb2610dd8B8f2871E160bd7',
-//     INTEREST_RATE_MODEL: '0xC862A3af64a8d3C146E6c505a18c2B6c6a6601bf',
-//     MERC_20_IMPL: '0x45d17FE87e65064b2e85F91A9FF3aD0C7B6Cf75d',
-//
-//     MARKETS: {
-//         "USDC.multi": {
-//             name: "USD Coin",
-//             assetTicker: "USDC",
-//             tokenAddress: '0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D',
-//             mTokenAddress: "0xd0670AEe3698F66e2D4dAf071EB9c690d978BFA8",
-//             digits: 6,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e6),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "ETH.multi": {
-//             name: "Ethereum",
-//             assetTicker: "ETH",
-//             tokenAddress: '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C',
-//             mTokenAddress: "0x6503D905338e2ebB550c9eC39Ced525b612E77aE",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e18),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "BTC.multi": {
-//             name: "Bitcoin",
-//             assetTicker: "BTC",
-//             tokenAddress: '0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8',
-//             mTokenAddress: "0x6E745367F4Ad2b3da7339aee65dC85d416614D90",
-//             digits: 8,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e8),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "MOVR": {
-//             name: "Moonriver",
-//             assetTicker: "MOVR",
-//             tokenAddress: null,
-//             mTokenAddress: "0x6a1A771C7826596652daDC9145fEAaE62b1cd07f",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e18),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "xcKSM": {
-//             name: "Kusama",
-//             assetTicker: "xcKSM",
-//             tokenAddress: '0xffffffff1fcacbd218edc0eba20fc2308c778080',
-//             mTokenAddress: '0xa0D116513Bd0B8f3F14e6Ea41556c6Ec34688e0f',
-//             digits: 12,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e12),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "FRAX": {
-//             name: "Frax",
-//             assetTicker: "FRAX",
-//             tokenAddress: '0x1A93B23281CC1CDE4C4741353F3064709A16197d',
-//             mTokenAddress: "0x93Ef8B7c6171BaB1C0A51092B2c9da8dc2ba0e9D",
-//             digits: 18,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e18),
-//             mTokenMantissa: new BigNumber(1e8),
-//         },
-//         "USDT.multi": {
-//             name: "Tether",
-//             assetTicker: "USDT",
-//             tokenAddress: '0xB44a9B6905aF7c801311e8F4E76932ee959c663C',
-//             mTokenAddress: "0x36918B66F9A3eC7a59d0007D8458DB17bDffBF21",
-//             digits: 6,
-//             mTokenDigits: 8,
-//             mantissa: new BigNumber(1e6),
-//             mTokenMantissa: new BigNumber(1e8),
-//         }
-//     }
-// }
+export const contracts: ContractBundle = {
+  CLAIMS: new MoonwellContractWithProxy(
+      '0x8568A675384d761f36eC269D695d6Ce4423cfaB1',
+      './deploy-artifacts/TokenSaleDistributor.json',
+      './deploy-artifacts/TokenSaleDistributorProxy.json',
+  ),
+  GOVERNOR: new MoonwellContract(
+      '0x2BE2e230e89c59c8E20E633C524AD2De246e7370',
+      './deploy-artifacts/MoonwellGovernorArtemis.json',
+  ),
+
+  COMPTROLLER: new MoonwellContractWithProxy(
+      '0x0b7a0EAA884849c6Af7a129e899536dDDcA4905E', 
+      './deploy-artifacts/Comptroller.json',
+      './deploy-artifacts/Unitroller.json',
+  ),
+
+
+  GOV_TOKEN:  new MoonwellContract(
+      '0xBb8d88bcD9749636BC4D2bE22aaC4Bb3B01A58F1', 
+      './deploy-artifacts/Well.json'
+  ),
+
+
+  MAXIMILLION: new MoonwellContract(
+      '0x1650C0AD9483158f9e240fd58d0E173807A80CcC', 
+      './deploy-artifacts/Maximillion.json'
+  ),
+
+  ORACLE: new MoonwellContract(
+      '0x892bE716Dcf0A6199677F355f45ba8CC123BAF60', 
+      './deploy-artifacts/ChainlinkOracle.json'
+  ),
+
+
+  SAFETY_MODULE: new MoonwellContractWithProxy(
+      '0xCd76e63f3AbFA864c53b4B98F57c1aA6539FDa3a', 
+      './deploy-artifacts/StakedWell.json',
+      './deploy-artifacts/TransparentProxy.json',
+  ),
+
+  TIMELOCK: new MoonwellContract(
+      '0x04e6322D196E0E4cCBb2610dd8B8f2871E160bd7', 
+      './deploy-artifacts/Timelock.json',
+  ),
+
+  INTEREST_RATE_MODEL: new MoonwellContract(
+      '0xC862A3af64a8d3C146E6c505a18c2B6c6a6601bf', 
+      './deploy-artifacts/StakedWell.json',
+  ),
+
+  MERC_20_IMPL: new MoonwellContract(
+      '0x45d17FE87e65064b2e85F91A9FF3aD0C7B6Cf75d', 
+      './deploy-artifacts/MErc20Delegator.json',
+  ),
+
+    MARKETS: {
+        "USDC.multi": {
+            name: "USD Coin",
+            assetTicker: "USDC",
+            tokenAddress: '0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D',
+            mTokenAddress: "0xd0670AEe3698F66e2D4dAf071EB9c690d978BFA8",
+            digits: 6,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e6),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "ETH.multi": {
+            name: "Ethereum",
+            assetTicker: "ETH",
+            tokenAddress: '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C',
+            mTokenAddress: "0x6503D905338e2ebB550c9eC39Ced525b612E77aE",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e18),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "BTC.multi": {
+            name: "Bitcoin",
+            assetTicker: "BTC",
+            tokenAddress: '0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8',
+            mTokenAddress: "0x6E745367F4Ad2b3da7339aee65dC85d416614D90",
+            digits: 8,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e8),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "MOVR": {
+            name: "Moonriver",
+            assetTicker: "MOVR",
+            tokenAddress: null,
+            mTokenAddress: "0x6a1A771C7826596652daDC9145fEAaE62b1cd07f",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e18),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "xcKSM": {
+            name: "Kusama",
+            assetTicker: "xcKSM",
+            tokenAddress: '0xffffffff1fcacbd218edc0eba20fc2308c778080',
+            mTokenAddress: '0xa0D116513Bd0B8f3F14e6Ea41556c6Ec34688e0f',
+            digits: 12,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e12),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "FRAX": {
+            name: "Frax",
+            assetTicker: "FRAX",
+            tokenAddress: '0x1A93B23281CC1CDE4C4741353F3064709A16197d',
+            mTokenAddress: "0x93Ef8B7c6171BaB1C0A51092B2c9da8dc2ba0e9D",
+            digits: 18,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e18),
+            mTokenMantissa: new BigNumber(1e8),
+        },
+        "USDT.multi": {
+            name: "Tether",
+            assetTicker: "USDT",
+            tokenAddress: '0xB44a9B6905aF7c801311e8F4E76932ee959c663C',
+            mTokenAddress: "0x36918B66F9A3eC7a59d0007D8458DB17bDffBF21",
+            digits: 6,
+            mTokenDigits: 8,
+            mantissa: new BigNumber(1e6),
+            mTokenMantissa: new BigNumber(1e8),
+        }
+    }
+}
