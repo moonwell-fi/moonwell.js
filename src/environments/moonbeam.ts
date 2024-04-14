fa85185b243ed672b68fde7dc9087b8ea312450a
Luke Youngblood <luke@moonwell.fi>
2024-04-14 14:28:03 -0700

    Fix timelock address to multichain governor

--- src/environments/moonbeam.ts
+++ src/environments/moonbeam.ts
@@ -61,7 +61,7 @@ export const contracts: ContractBundle = {
     ),
 
     TIMELOCK: new MoonwellContract<types.Timelock>(
-        '0x3a9249d70dCb4A4E9ef4f3AF99a3A130452ec19B',
+        '0x9A8464C4C11CeA17e191653Deb7CdC1bE30F1Af4', // Note: same as multichain governor
         Timelock,
     ),
 
