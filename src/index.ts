25b621383d5301b4c9680050a722019332e334ab
Leonardo Nagasaki <56836200+Leoakin43@users.noreply.github.com>
2024-07-26 14:48:17 -0300

    Fixed Optimism configurations

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import { environment as MoonriverEnvironment } from './environments/moonriver'
 import { environment as MoonbaseEnvironment } from './environments/moonbase'
 import { environment as BaseEnvironment } from './environments/base'
 import { environment as BaseSepoliaEnvironment } from './environments/basesepolia'
+import { environment as OptimismEnvironment } from './environments/optimism'
+import { environment as OptimismSepoliaEnvironment } from './environments/optimismsepolia'
 
 import { Environment } from "./types";
 
@@ -21,6 +23,8 @@ export const Environments = {
   [Environment.MOONBASE]: MoonbaseEnvironment,
   [Environment.BASE]: BaseEnvironment,
   [Environment.BASESEPOLIA]: BaseSepoliaEnvironment,
+  [Environment.OPTIMISM]: OptimismEnvironment,
+  [Environment.OPTIMISMSEPOLIA]: OptimismSepoliaEnvironment,
 }
 
 export * from './types'
